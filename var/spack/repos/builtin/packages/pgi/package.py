5a8b238b1cfb9a9bb1b4d46533e6328f2e4f3af0
Pramod S Kumbhar <pramod.s.kumbhar@gmail.com>
2018-03-12 21:17:32 +0100

    Changes incorporated initial private fork : https://github.com/pramodskumbhar/spack.git

--- var/spack/repos/builtin/packages/pgi/package.py
+++ var/spack/repos/builtin/packages/pgi/package.py
@@ -23,7 +23,7 @@ class Pgi(Package):
     version('19.1', sha256='3e05a6db2bf80b5d15f6ff83188f20cb89dc23e233417921e5c0822e7e57d34f')
     version('18.10', sha256='4b3ff83d2a13de6001bed599246eff8e63ef711b8952d4a9ee12efd666b3e326')
     version('18.4',  'b55461f9f0986acbd51902c51c2074b9')
-    version('17.10', '85ad6506e7ada610ab11ddb35d697efa')
+    version('17.10', '33e407eea9600997914b2535a36b4d92')
     version('17.4',  'a311d2756ddda657860bad8e5725597b')
     version('17.3',  '6eefc42f85e756cbaba76467ed640902')
     version('16.10', '9bb6bfb7b1052f9e6a45829ba7a24e47')
