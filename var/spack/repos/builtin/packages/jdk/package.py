c5036fe8f836f55412394dcb81a84e6ab2a6b4f9
Pramod Kumbhar <pramod.s.kumbhar@gmail.com>
2018-10-20 18:36:25 +0200

    Delete java@10 and java@9 provider (#115)
    * update v8 with checksum

--- var/spack/repos/builtin/packages/jdk/package.py
+++ var/spack/repos/builtin/packages/jdk/package.py
@@ -60,9 +60,6 @@ class Jdk(Package):
     version('1.7.0_80-b0', '6152f8a7561acf795ca4701daa10a965', curl_options=curl_options)
 
     provides('java')
-    provides('java@11', when='@11.0:11.999')
-    provides('java@10', when='@10.0:10.999')
-    provides('java@9',  when='@9.0:9.999')
     provides('java@8',  when='@1.8.0:1.8.999')
     provides('java@7',  when='@1.7.0:1.7.999')
 
