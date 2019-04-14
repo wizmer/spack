6e2857affaa9764ebbcccd65f13befafa2756a7f
Pramod Kumbhar <pramod.s.kumbhar@gmail.com>
2019-04-14 09:26:46 +0200

    Use spack_env to set all env variables

--- var/spack/repos/builtin/packages/tensorflow/package.py
+++ var/spack/repos/builtin/packages/tensorflow/package.py
@@ -73,91 +73,10 @@ class Tensorflow(Package):
 
 
     def install(self, spec, prefix):
-        if '+gcp' in spec:
-            env['TF_NEED_GCP'] = '1'
-        else:
-            env['TF_NEED_GCP'] = '0'
-
-        env['PYTHON_BIN_PATH'] = str(spec['python'].prefix.bin) + '/python'
-        env['PYTHONUSERBASE'] = str(spec['python'].prefix)
-        env['USE_DEFAULT_PYTHON_LIB_PATH'] = '1'
-        env['SWIG_PATH'] = str(spec['swig'].prefix.bin)
-        env['GCC_HOST_COMPILER_PATH'] = spack_cc
-
-        env['TF_ENABLE_XLA'] = '0'
-
-        if '+cuda' in spec:
-            env['TF_NEED_CUDA'] = '1'
-            env['TF_CUDA_CLANG'] = '0'
-            env['TF_NEED_TENSORRT'] = '0'
-            env['TF_CUDA_VERSION'] = str(spec['cuda'].version)
-            env['CUDA_TOOLKIT_PATH'] = str(spec['cuda'].prefix)
-            env['TF_CUDNN_VERSION'] = str(spec['cudnn'].version)[0]
-            env['CUDNN_INSTALL_PATH'] = str(spec['cudnn'].prefix)
-            env['NCCL_INSTALL_PATH'] = str(spec['nccl'].prefix)
-            env['TF_NCCL_VERSION'] = str(spec['nccl'].version)[0]
-            env['TF_CUDA_COMPUTE_CAPABILITIES'] = '6.0,6.1,7.0'
-        else:
-            env['TF_NEED_CUDA'] = '0'
-            env['TF_CUDA_VERSION'] = ''
-            env['CUDA_TOOLKIT_PATH'] = ''
-            env['TF_CUDNN_VERSION'] = ''
-            env['CUDNN_INSTALL_PATH'] = ''
-
-        # additional config options starting with version 1.2
-        if self.spec.satisfies('@1.2.0:'):
-            env['TF_NEED_MKL'] = '1'
-            env['TF_NEED_VERBS'] = '1'
-
-        # additional config options starting with version 1.3
-        if self.spec.satisfies('@1.3.0:'):
-            env['TF_NEED_MPI'] = '0'
-
-        # additional config options starting with version 1.5
-        if self.spec.satisfies('@1.5.0:'):
-            env['TF_NEED_S3'] = '0'
-            env['TF_NEED_GDR'] = '0'
-            env['TF_NEED_OPENCL_SYCL'] = '0'
-            env['TF_SET_ANDROID_WORKSPACE'] = '0'
-            # env variable is somehow ignored -> brute force
-            filter_file(r'if workspace_has_any_android_rule\(\)', r'if True', 'configure.py')
-
-        # additional config options starting with version 1.6
-        if self.spec.satisfies('@1.6.0:'):
-            env['TF_NEED_KAFKA'] = '0'
-
-        # additional config options starting with version 1.8
-        if self.spec.satisfies('@1.8.0:'):
-            env['TF_DOWNLOAD_CLANG'] = '0'
-            env['TF_NEED_AWS'] = '0'
-
-        # boringssl error again, build against openssl instead via TF_SYSTEM_LIBS
-        # does not work for tf < 1.12.0
-        # (https://github.com/tensorflow/tensorflow/issues/25283#issuecomment-460124556)
-        if self.spec.satisfies('@1.12.0:'):
-            env['TF_SYSTEM_LIBS'] = "boringssl"
-            env['TF_NEED_IGNITE'] = '0'
-            env['TF_NEED_ROCM'] = '0'
-
-        # set tmpdir to a non-NFS filesystem (because bazel uses ~/.cache/bazel)
-        # TODO: This should be checked for non-nfsy filesystem, but the current
-        #       best idea for it is to check
-        #           subprocess.call(['stat', '--file-system', '--format=%T', tmp_path])
-        #       to not be nfs. This is only valid for Linux and we'd like to
-        #       stay at least also OSX compatible
-        # Note: This particular path below /tmp/spack/tmp is required by the visionary container
-        #       build flow:
-        # Note: On BB5 if we use /tmp as TMPDIR then we get "java.io.IOException: No space left on device"
-        # tmp_path = env.get('SPACK_TMPDIR', '/tmp/spack') + '/tf'
-        # mkdirp(tmp_path)
-        # env['TEST_TMPDIR'] = tmp_path
-        # env['HOME'] = tmp_path
-
         configure()
-
-
+        spec = self.spec
         # version dependent fixes
-        if self.spec.satisfies('@1.3.0:1.5.0'):
+        if spec.satisfies('@1.3.0:1.5.0'):
             # checksum for protobuf that bazel downloads (@github) changed, comment out to avoid error
             # better solution: replace wrong checksums in workspace.bzl
             # wrong one: 6d43b9d223ce09e5d4ce8b0060cb8a7513577a35a64c7e3dad10f0703bf3ad93,
@@ -171,19 +90,19 @@ class Tensorflow(Package):
             filter_file(r"'tensorflow-tensorboard",
                         r"#'tensorflow-tensorboard",
                         'tensorflow/tools/pip_package/setup.py')
-        if self.spec.satisfies('@1.5.0:'):
+        if spec.satisfies('@1.5.0:'):
             # google cloud support seems to be installed on default, leading to boringssl error
             # manually set the flag to false to avoid installing gcp support
             # (https://github.com/tensorflow/tensorflow/issues/20677#issuecomment-404634519)
             filter_file(r'--define with_gcp_support=true',
                         r'--define with_gcp_support=false',
                         '.tf_configure.bazelrc')
-        if self.spec.satisfies('@1.6.0:'):
+        if spec.satisfies('@1.6.0:'):
             # tensorboard name changed
             filter_file(r"'tensorboard >=",
                         r"#'tensorboard >=",
                         'tensorflow/tools/pip_package/setup.py')
-        if self.spec.satisfies('@1.8.0:'):
+        if spec.satisfies('@1.8.0:'):
             # 1.8.0 and 1.9.0 aborts with numpy import error during python_api generation
             # somehow the wrong PYTHONPATH is used...set --distinct_host_configuration=false as a workaround
             # (https://github.com/tensorflow/tensorflow/issues/22395#issuecomment-431229451)
@@ -193,10 +112,10 @@ class Tensorflow(Package):
                         'build --action_env PYTHONPATH="{}"'.format(env['PYTHONPATH']),
                         '.tf_configure.bazelrc')
 
-        if self.spec.satisfies('@1.12.0:'):
+        if spec.satisfies('@1.12.0:'):
             # add link to spack-installed openssl libs (needed if no system openssl available)
-            filter_file('-lssl', '-lssl '+self.spec['openssl'].libs.search_flags, 'third_party/systemlibs/boringssl.BUILD')
-            filter_file('-lcrypto', '-lcrypto '+self.spec['openssl'].libs.search_flags, 'third_party/systemlibs/boringssl.BUILD')
+            filter_file('-lssl', '-lssl '+spec['openssl'].libs.search_flags, 'third_party/systemlibs/boringssl.BUILD')
+            filter_file('-lcrypto', '-lcrypto '+spec['openssl'].libs.search_flags, 'third_party/systemlibs/boringssl.BUILD')
 
         if '+cuda' in spec:
             # get path for all dependent libraries to avoid issue with linking esp cudnn
@@ -221,7 +140,89 @@ class Tensorflow(Package):
             ln('-s', fn, '.')
         setup_py('install', '--prefix={0}'.format(prefix))
 
+
     def setup_environment(self, spack_env, run_env):
+        spec = self.spec
         # Note : do not enable skylake yet, see https://github.com/easybuilders/easybuild-easyconfigs/issues/5936
         opt_flags = "--copt=-mavx --copt=-mavx2 --copt=-mfma --copt=-msse4.2 --copt=-mfpmath=both"
         spack_env.set('CC_OPT_FLAGS', opt_flags)
+
+        if '+gcp' in spec:
+            spack_env.set('TF_NEED_GCP', '1')
+        else:
+            spack_env.set('TF_NEED_GCP', '0')
+
+        spack_env.set('PYTHON_BIN_PATH', str(spec['python'].prefix.bin) + '/python')
+        spack_env.set('PYTHONUSERBASE', str(spec['python'].prefix))
+        spack_env.set('USE_DEFAULT_PYTHON_LIB_PATH', '1')
+        spack_env.set('SWIG_PATH', str(spec['swig'].prefix.bin))
+        spack_env.set('GCC_HOST_COMPILER_PATH', spack_cc)
+
+        spack_env.set('TF_ENABLE_XLA', '0')
+
+        if '+cuda' in spec:
+            spack_env.set('TF_NEED_CUDA', '1')
+            spack_env.set('TF_CUDA_CLANG', '0')
+            spack_env.set('TF_NEED_TENSORRT', '0')
+            spack_env.set('TF_CUDA_VERSION', str(spec['cuda'].version))
+            spack_env.set('CUDA_TOOLKIT_PATH', str(spec['cuda'].prefix))
+            spack_env.set('TF_CUDNN_VERSION', str(spec['cudnn'].version)[0])
+            spack_env.set('CUDNN_INSTALL_PATH', str(spec['cudnn'].prefix))
+            spack_env.set('NCCL_INSTALL_PATH', str(spec['nccl'].prefix))
+            spack_env.set('TF_NCCL_VERSION', str(spec['nccl'].version)[0])
+            spack_env.set('TF_CUDA_COMPUTE_CAPABILITIES', '6.0,6.1,7.0')
+        else:
+            spack_env.set('TF_NEED_CUDA', '0')
+            spack_env.set('TF_CUDA_VERSION', '')
+            spack_env.set('CUDA_TOOLKIT_PATH', '')
+            spack_env.set('TF_CUDNN_VERSION', '')
+            spack_env.set('CUDNN_INSTALL_PATH', '')
+
+        # additional config options starting with version 1.2
+        if spec.satisfies('@1.2.0:'):
+            spack_env.set('TF_NEED_MKL', '1')
+            spack_env.set('TF_NEED_VERBS', '1')
+
+        # additional config options starting with version 1.3
+        if spec.satisfies('@1.3.0:'):
+            spack_env.set('TF_NEED_MPI', '0')
+
+        # additional config options starting with version 1.5
+        if spec.satisfies('@1.5.0:'):
+            spack_env.set('TF_NEED_S3', '0')
+            spack_env.set('TF_NEED_GDR', '0')
+            spack_env.set('TF_NEED_OPENCL_SYCL', '0')
+            spack_env.set('TF_SET_ANDROID_WORKSPACE', '0')
+            # env variable is somehow ignored -> brute force
+            # filter_file(r'if workspace_has_any_android_rule\(\)', r'if True', 'configure.py')
+
+        # additional config options starting with version 1.6
+        if spec.satisfies('@1.6.0:'):
+            spack_env.set('TF_NEED_KAFKA', '0')
+
+        # additional config options starting with version 1.8
+        if spec.satisfies('@1.8.0:'):
+            spack_env.set('TF_DOWNLOAD_CLANG', '0')
+            spack_env.set('TF_NEED_AWS', '0')
+
+        # boringssl error again, build against openssl instead via TF_SYSTEM_LIBS
+        # does not work for tf < 1.12.0
+        # (https://github.com/tensorflow/tensorflow/issues/25283#issuecomment-460124556)
+        if spec.satisfies('@1.12.0:'):
+            spack_env.set('TF_SYSTEM_LIBS', "boringssl")
+            spack_env.set('TF_NEED_IGNITE', '0')
+            spack_env.set('TF_NEED_ROCM', '0')
+
+        # set tmpdir to a non-NFS filesystem (because bazel uses ~/.cache/bazel)
+        # TODO: This should be checked for non-nfsy filesystem, but the current
+        #       best idea for it is to check
+        #           subprocess.call(['stat', '--file-system', '--format=%T', tmp_path])
+        #       to not be nfs. This is only valid for Linux and we'd like to
+        #       stay at least also OSX compatible
+        # Note: This particular path below /tmp/spack/tmp is required by the visionary container
+        #       build flow:
+        # Note: On BB5 if we use /tmp as TMPDIR then we get "java.io.IOException: No space left on device"
+        # tmp_path = env.get('SPACK_TMPDIR', '/tmp/spack') + '/tf'
+        # mkdirp(tmp_path)
+        # spack_env.set('TEST_TMPDIR', tmp_path)
+        # spack_env.set('HOME', tmp_path)
