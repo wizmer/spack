4ad03a6f0efffd8d297c8ee7d4ef9a16e32cae5b
Benoit Coste <benoit.coste@epfl.ch>
2020-01-29 18:35:00 +0100

    Bump scikit-learn and add py-bbp-morphology-workflow

---
+++ var/spack/repos/builtin/packages/py-bbp-morphology-workflow/package.py
@@ -0,0 +1,27 @@
+# Copyright 2013-2018 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack import *
+
+
+class PyBbpMorphologyWorkflow(PythonPackage):
+    """Python library neuron morphology analysis"""
+
+    homepage = "https://bbpcode.epfl.ch/code/#/admin/projects/platform/bbp-morphology-workflow"
+    git      = "ssh://bbpcode.epfl.ch/platform/bbp-morphology-workflow"
+    version('develop', branch='master')
+    version('1.0.1', tag='morphology-repair-workflow-v1.0.1')
+
+    depends_on('py-setuptools', type=('build', 'run'))
+
+    depends_on('py-neuroc', type='run')
+    depends_on('py-numpy', type='run')
+    depends_on('py-nose', type='run')
+    depends_on('py-tqdm@4.23.4:', type='run')
+    depends_on('py-click', type='run')
+    depends_on('py-pathlib2', type='run')
+    depends_on('py-morphio', type='run')
+    depends_on('py-morph-tool', type='run')
+    depends_on('py-neurom', type='run')
