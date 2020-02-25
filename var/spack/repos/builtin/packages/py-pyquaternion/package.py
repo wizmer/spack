074b32503c70c2438404bcc7f5ca2cd43e18a79a
Benoît Coste <benoit.coste@epfl.ch>
2020-02-25 11:15:59 +0100

    Fix py-quaternion

--- var/spack/repos/builtin/packages/py-pyquaternion/package.py
+++ var/spack/repos/builtin/packages/py-pyquaternion/package.py
@@ -12,7 +12,6 @@ class PyPyquaternion(PythonPackage):
     homepage = "https://kieranwynn.github.io/pyquaternion/"
     url      = "https://files.pythonhosted.org/packages/ae/c8/02b30c4a86744d2e15f7f16ab353f7231bd0241117713e5d60f466044994/pyquaternion-0.9.5.tar.gz"
 
-    version('develop', branch='master')
     version('0.9.5', sha256='2d89d19259d62a8fbd25219eee7dacc1f6bb570becb70e1e883f622597c7d81d')
 
     depends_on('py-setuptools', type=('build', 'run'))
