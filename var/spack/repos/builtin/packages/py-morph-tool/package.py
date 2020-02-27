758cdc22b10cdfe9239b5bdc9862371222c802f9
Benoit Coste <benoit.coste@epfl.ch>
2020-02-27 11:57:02 +0100

    remove neurom@mut_morphio dep on morph-tool

--- var/spack/repos/builtin/packages/py-morph-tool/package.py
+++ var/spack/repos/builtin/packages/py-morph-tool/package.py
@@ -25,4 +25,4 @@ class PyMorphTool(PythonPackage):
     depends_on('py-pyyaml', type='run')
     depends_on('py-functools32', when='^python@:2.99', type='run')
     depends_on('py-pathlib2@2.3.5:', type='run')
-    depends_on('py-neurom@mut_morphio+plotly', type='run', when='@0.1.14:')
+    depends_on('py-neurom', type='run', when='@0.1.14:')
