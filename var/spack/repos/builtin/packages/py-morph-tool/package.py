69132f0e5e6b8bfa954c7dd1adf290c9025e6087
Benoit Coste <benoit.coste@epfl.ch>
2020-02-27 09:51:33 +0100

    WIP: adding morphology stack one package at a time: part 2

--- var/spack/repos/builtin/packages/py-morph-tool/package.py
+++ var/spack/repos/builtin/packages/py-morph-tool/package.py
@@ -1,4 +1,4 @@
-# Copyright 2013-2018 Lawrence Livermore National Security, LLC and other
+# Copyright 2013-2020 Lawrence Livermore National Security, LLC and other
 # Spack Project Developers. See the top-level COPYRIGHT file for details.
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
@@ -13,8 +13,8 @@ class PyMorphTool(PythonPackage):
     git      = "ssh://bbpcode.epfl.ch/nse/morph-tool"
 
     version('develop', branch='master')
+    version('0.2.1', tag='morph-tool-v0.2.1')
     version('0.1.14', tag='morph-tool-v0.1.14')
-    version('0.1.3', tag='morph-tool-v0.1.3')
 
     variant('neuron', default=False, description='Neuron-based functionality')
 
@@ -22,7 +22,7 @@ class PyMorphTool(PythonPackage):
 
     depends_on('py-bluepyopt', type='run', when='+neuron')
     depends_on('py-click', type='run')
+    depends_on('py-pyyaml', type='run')
     depends_on('py-functools32', when='^python@:2.99', type='run')
-    depends_on('py-morphio', type='run')
-    depends_on('py-numpy', type='run')
-    depends_on('py-neurom', type='run', when='@0.1.14:')
+    depends_on('py-pathlib2@2.3.5:', type='run')
+    depends_on('py-neurom@mut_morphio+plotly', type='run', when='@0.1.14:')
